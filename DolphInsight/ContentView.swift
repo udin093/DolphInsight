0dfedf26c3bc38defe9278bc08222e500198144d
M. Khalid Assiddiq <mahfuz.hidayah@gmail.com>
2024-05-24 20:34:43 +0700

    Initial Commit

---
+++ DolphInsight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DolphInsight
+//
+//  Created by M Khalid Assiddiq on 24/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DolphInsight/DolphInsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  DolphInsightApp.swift
+//  DolphInsight
+//
+//  Created by M Khalid Assiddiq on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DolphInsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
