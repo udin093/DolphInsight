ddfd8649a32f0d74e9bdf6164b578c61aaf0e8ab
Mahmud-Manoe <79903974+Mahmud-Manoe@users.noreply.github.com>
2024-05-24 22:53:51 +0700

    hahah

--- DolphInsight/ContentView.swift
+++ DolphInsight/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, Guys!")
             Text("Hello, bang!")
+            Text("Hello")
         }
         .padding()
     }
